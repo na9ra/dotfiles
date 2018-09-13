6fd2827b9ff3c129921b6f336766afb1ba3fcba8
na9ra <nakurasansan@gmail.com>
2018-09-14 03:02:59 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
+ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
 
-set -u
-DOT_DIRECTORY="${HOME}/dotfiles"
-DOT_CONFIG_DIRECTORY=".config"
-
-echo "link home directory dotfiles"
-cd ${DOT_DIRECTORY}
-for f in .??*
-do
-  [ "$f" == ".git" ] && continue
-  [ "$f" == ".config" ] && continue
-  ln -snfv ${DOT_DIRECTORY}${f} ${HOME}/${f}
-done
-
-echo "link .config directory dotfiles"
-cd ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}
-for file in `\find . -maxdepth 8 -type f`; do
-  ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}/${file:2} ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
-done
-
-echo "linked dotfiles complete!"
