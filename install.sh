c9e4b81db3cda0b494e91754688c86881bb0a503
na9ra <nakurasansan@gmail.com>
2018-09-14 03:10:12 +0900

    install.sh 2

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
 ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -sf ~/dotfiles/.xmobarrc ~/.xmobarrc
+ln -sf ~/dotfiles/.xprofile ~/.xprofile
+ln -sf ~/dotfiles/.xscreensaver ~/.xscreensaver
+
 
